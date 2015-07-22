064138f8aa14eef9d427b41bcced6742c31c5341
Felix Geyer <debfx@fobos.de>
2015-07-22 23:40:26 +0200

    Require libgcrypt >= 1.6.0.
    Remove our own Salsa20 implementation as libgcrypt ships one
    with that version.

--- src/crypto/Crypto.cpp
+++ src/crypto/Crypto.cpp
@@ -29,43 +29,6 @@ bool Crypto::m_initalized(false);
 QString Crypto::m_errorStr;
 QString Crypto::m_backendVersion;
 
-#if !defined(GCRYPT_VERSION_NUMBER) || (GCRYPT_VERSION_NUMBER < 0x010600)
-static int gcry_qt_mutex_init(void** p_sys)
-{
-    *p_sys = new QMutex();
-    return 0;
-}
-
-static int gcry_qt_mutex_destroy(void** p_sys)
-{
-    delete reinterpret_cast<QMutex*>(*p_sys);
-    return 0;
-}
-
-static int gcry_qt_mutex_lock(void** p_sys)
-{
-    reinterpret_cast<QMutex*>(*p_sys)->lock();
-    return 0;
-}
-
-static int gcry_qt_mutex_unlock(void** p_sys)
-{
-    reinterpret_cast<QMutex*>(*p_sys)->unlock();
-    return 0;
-}
-
-static const struct gcry_thread_cbs gcry_threads_qt =
-{
-    GCRY_THREAD_OPTION_USER,
-    0,
-    gcry_qt_mutex_init,
-    gcry_qt_mutex_destroy,
-    gcry_qt_mutex_lock,
-    gcry_qt_mutex_unlock,
-    0, 0, 0, 0, 0, 0, 0, 0
-};
-#endif
-
 Crypto::Crypto()
 {
 }
@@ -82,6 +45,7 @@ bool Crypto::init()
     gcry_control(GCRYCTL_SET_THREAD_CBS, &gcry_threads_qt);
 #endif
     m_backendVersion = QString::fromLocal8Bit(gcry_check_version(0));
+    gcry_check_version(0);
     gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0);
 
     if (!checkAlgorithms()) {
